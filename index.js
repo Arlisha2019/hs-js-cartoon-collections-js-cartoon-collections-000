a9df8bdf13f8650e88af67d2c9f7b27dd0101e8f
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-25 01:17:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+  var list = "<ol>";
+for (var i = 0; i < dwarves.length; i++) {
+  list =+ "<li>" + dwarves[i] + "</li>";
+  }  
+  return 
 }
 
 function summonCaptainPlanet(planeteerCalls){
