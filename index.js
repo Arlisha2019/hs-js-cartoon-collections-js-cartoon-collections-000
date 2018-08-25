a37d1519f925c7c7d02be5c1c660c172a70cb984
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-25 23:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ if ( words.length > 4) {
   return list;
 } else {
   return false;
+ }
 }
-}
-
-
-
-function findTheCheese (foods) {
-}
+ 
+// function findTheCheese (foods) {
+//   let cheese = ['cheddar', 'gouda', 'camembert'];
+// for (var i = 0; i < foods.length; i++) {
+//   for (var j = 0; i < cheese.length; j++) {
+//     if(cheese[j] === foods[i]) {
+//       return foods[i];
+// }
+//   }
+//     }
+//       return 'no cheese!';
+// }
