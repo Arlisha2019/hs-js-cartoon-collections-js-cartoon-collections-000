c668671905abfdaf570ce33a865e3830a9b33525
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-25 18:01:31 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ function dwarfRollCall(dwarves) {
   var list = '';
 for (var i = 0; i < dwarves.length; i++) {
   list += (i + 1) + '. ' + dwarves[i] + ' ';
-  }  
+ }  
   return list;
 }
 
 function summonCaptainPlanet(planeteerCalls) {
-  
+  var list = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+   list.push(planeteerCalls[i].toUpperCase() + ' ! ') ;
+}
+ return list;
 }
 
 function longPlaneteerCalls(words) {
